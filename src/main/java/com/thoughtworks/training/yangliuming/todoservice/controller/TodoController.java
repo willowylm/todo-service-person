7e7eaeb74214fd3275852f0448ef898713a4dc60
Liuming Yang <ylmap@126.com>
2018-08-01 08:44:39 +0800

    add @Controller

--- src/main/java/com/thoughtworks/training/yangliuming/todoservice/controller/TodoController.java
+++ src/main/java/com/thoughtworks/training/yangliuming/todoservice/controller/TodoController.java
@@ -3,11 +3,13 @@ package com.thoughtworks.training.yangliuming.todoservice.controller;
 import com.thoughtworks.training.yangliuming.todoservice.model.Task;
 import com.thoughtworks.training.yangliuming.todoservice.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class TodoController {
 
     @Autowired
